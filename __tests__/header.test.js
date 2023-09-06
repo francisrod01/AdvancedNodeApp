0f2d393f51dd68e79eca0d49b1810eabe4f2e337
Francis Rodrigues <francisrod01@users.noreply.github.com>
2023-09-05 23:26:06 -0300

    tests: implement header's oauth and logout

---
+++ __tests__/header.test.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+
+test("We can launch a browser", async () => {
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+
+  expect(text).toEqual("Blogster");
+});
+
+test("clicking Log in starts OAuth flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test("When signed in, shows logout button", async () => {
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
+
+  await page.setCookie({ name: 'session', value: session });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('http://localhost:3000');
+  await page.waitForSelector('a[href="/auth/logout"]');
+
+  const logoutText = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  expect(logoutText).toEqual('Logout');
+});
