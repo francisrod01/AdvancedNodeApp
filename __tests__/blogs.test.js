6dd464c31065636f2605673cb516c178ea235b20
Francis Rodrigues <francisrod01@users.noreply.github.com>
2023-09-09 13:52:07 -0300

    tests: checks blog posts validation error messages

--- __tests__/blogs.test.js
+++ __tests__/blogs.test.js
@@ -50,4 +50,18 @@ describe('When logged in', async () => {
       expect(content).toEqual('My content..');
     });
   });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });
