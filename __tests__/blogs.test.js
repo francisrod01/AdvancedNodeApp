d02da5a3b57c481e1e58094990934710afc40c17
Francis Rodrigues <francisrod01@users.noreply.github.com>
2023-09-09 13:49:32 -0300

    tests: blog posts creation when logged in

---
+++ __tests__/blogs.test.js
@@ -0,0 +1,28 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  test('When logged in, can see blog create form', async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+
+    const label = await page.getContentsOf('form label');
+
+    expect(label).toEqual('Blog Title');
+  });
+});
