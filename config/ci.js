da40f7fadaf645be5b4cce6bda1ab61b30ef63e7
Francis Rodrigues <francisrod01@users.noreply.github.com>
2023-09-10 16:36:09 -0300

    chore: sets up a CI config file

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '<CLIENT_ID>.apps.googleusercontent.com',
+  googleClientSecret: '<CLIENT_SECRET>',
+  mongoURI:
+    'mongodb://127.0.0.1:27017/blog_ci_123000',
+  cookieKey: '123123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+};
