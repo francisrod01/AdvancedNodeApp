c59737c0782a5876a52dfbef11338ce917245d37
Francis Rodrigues <francisrod01@users.noreply.github.com>
2023-09-12 11:40:47 -0300

    feat(react BlogShow): renders image from S3 bucket

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,13 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      const url = 'https://s3-us-west-2.amazonaws.com/my-blog-bucket-123/';
+      return <img src={url + this.props.blog.imageUrl} />
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +25,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
